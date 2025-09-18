9f7b3caeebf6187f6f4abdb9f4f605b247b6c407
Enzo Marx <161323562+enzomarx@users.noreply.github.com>
2025-09-18 14:55:53 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,35 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import path from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
+export default defineConfig({
+  base: './', // garante que os assets funcionem no deploy
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './'),
+    },
+  },
+})
+
+
+///
+//import path from 'path';
+//import { defineConfig, loadEnv } from 'vite';
+//import react from '@vitejs/plugin-react';
+
+//export default defineConfig(({ mode }) => {
+    //const env = loadEnv(mode, '.', '');
+    //return {
+      //plugins: [react()],
+      //define: {
+        //'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+        //'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+      //},
+      //resolve: {
+        //alias: {
+          //'@': path.resolve(__dirname, '.'),
+        //}
+      //}
+    //};
+//});
